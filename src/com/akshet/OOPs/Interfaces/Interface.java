add8d2539f9f6ab5dfda1eabb9f08c4042316cc1
AKSHET PATIAL <akshet.patial@gmail.com>
2023-04-25 00:25:10 +0530

    Added the interface and implments those interfaces from a class to apply

--- src/com/akshet/OOPs/Interfaces/Interface.java
+++ src/com/akshet/OOPs/Interfaces/Interface.java
@@ -24,16 +24,19 @@ interface Window{
     public void down();
 }
 
-class Car implements Engine,Music_Player,AC,Brake,Window{
+// Here what ever we are defining it will call that no matter even if we have many methods with same name
+
+// Instead we can have a separate class to implement interfaces
+class Car implements Engine,Music_Player,AC,Brake,Window {
 
     @Override
     public void start() {
-        System.out.println("start");
+        System.out.println("start the car");
     }
 
     @Override
     public void stop() {
-        System.out.println("stop");
+        System.out.println("stop the car");
     }
 
     @Override
@@ -50,12 +53,33 @@ class Car implements Engine,Music_Player,AC,Brake,Window{
     public void down() {
         System.out.println("Window down");
     }
+
 }
+
+    class mediaPlayer implements Music_Player{
+
+        @Override
+        public void start() {
+            System.out.println("Start the music player");
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("Stop the music player");
+        }
+    }
+
 
 public class Interface {
     public static void main(String[] args) {
         System.out.println("Interface! ");
         Car newCar= new Car();
-        
+        newCar.start();;
+        newCar.stop();
+
+        mediaPlayer mp = new mediaPlayer();
+        mp.start();
+        mp.stop();
+
     }
 }
